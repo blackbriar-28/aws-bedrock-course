8f9b45bfffa51c6deed218cb5b455037d7df6358
blackbriar-28 <highstuff10@gmail.com>
2025-10-03 23:41:42 -0600

    Adding requirements and first script

---
+++ src/intro/starter.py
@@ -0,0 +1,19 @@
+import boto3
+import pprint
+
+bedrock = boto3.client(service_name='bedrock', region_name='us-west-2')
+
+pp = pprint.PrettyPrinter(indent=4).pprint
+
+def list_foundation_models():
+  models = bedrock.list_foundation_models()
+  for model in models['modelSummaries']:
+      pp(model)
+      pp("----------------------------")
+      
+def get_foundation_model(modelIdentifier):
+  model = bedrock.get_foundation_model(modelIdentifier=modelIdentifier)
+  pp(model)      
+
+# list_foundation_models()
+get_foundation_model(modelIdentifier='mistral.mistral-large-2402-v1:0')
